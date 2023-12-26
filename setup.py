177628cecc70217ab1b503a335d524495295823e
youliang <tan_you_liang@hotmail.com>
2023-12-25 21:24:28 -0800

    nit setup.py
    Signed-off-by: youliang <tan_you_liang@hotmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     packages=find_packages(),
     description='library to enable distributed edge ml training and inference',
     url='https://github.com/youliangtan/edgeml',
-    packages=find_packages(),
     author='auth',
     author_email='tan_you_liang@hotmail.com',
     license='MIT',
